ce5e6d9c91f43e45e49d44146343aa5a08e34e13
Jericoth Castellar <143036192+JericothCastellar@users.noreply.github.com>
2025-08-17 14:59:05 -0500

    Add files via upload
    2#
    Organizcion y verificacion correcta

---
+++ PruebasUnitarias/src/main/java/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leofa
+ */
+public class Calculadora {
+    public int sumar (int a, int b){
+        return a+b;
+        
+    }
+    public int dividir (int a, int b){
+        if (b==0) throw new IllegalArgumentException("No se puede dividir entre cero");
+        return a/b;
+    }
+}
---
+++ PruebasUnitarias/src/main/java/PruebasUnitarias.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author leofa
+ */
+public class PruebasUnitarias {
+
+    public static void main(String[] args) {
+      
+    }
+}
